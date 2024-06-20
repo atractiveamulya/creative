f703b045ae29fa31dc8cb7ab03a55c533ef516de
AmulyaDalini <154538984+atractiveamulya@users.noreply.github.com>
2024-06-20 15:32:02 +0530

    Add files via upload

---
+++ array/SportMania.java
@@ -0,0 +1,13 @@
+class SportsMania{
+static String teamNames[]={"RCB","CSK","sri lanka","thailand","kuwait","pakistan","australian mens","bangladesh","west indies","enggland"};
+static String top10BatsMen[]={"baber Azam","shubman gill","virat kohli","rohit sharma","harry tector","dary mitchell","david warner","pathum nissanka","dawid","rassie van der dussen"};
+static String top10Bowlers[]={"keshav maharaj","josh","adam zampa","mohammed siraj","jasprit bumbah","mohammad nabi","shaheen afridi","kuldeep yadav","trent boult","brenard scholtz"};
+static String ipltop10TeamNames[]={"RCB","chennai super lings","Delhi capitals","gujarat titans","kolkata knight riders","lucknow super giants","mumbai indians","rajasthan royals","punjab kings","sunrisers hyderabad"};
+public static void main(String [] args){
+System.out.println("the top ten sport teams are:"+"\n"+teamNames[0]+"\n"+teamNames[1]+"\n"+teamNames[2]+"\n"+teamNames[3]+"\n"+teamNames[4]+"\n"+teamNames[5]+"\n"+teamNames[6]+"\n"+teamNames[7]+"\n"+teamNames[8]+"\n"+teamNames[9]);
+System.out.println("the top ten batsmans are:"+"\n"+top10BatsMen[0]+"\n"+top10BatsMen[1]+"\n"+top10BatsMen[2]+"\n"+top10BatsMen[3]+"\n"+top10BatsMen[4]+"\n"+top10BatsMen[5]+"\n"+top10BatsMen[6]+"\n"+top10BatsMen[7]+"\n"+top10BatsMen[8]+"\n"+top10BatsMen[9]);
+System.out.println("the top ten bbowlers are:"+"\n"+top10Bowlers[0]+"\n"+top10Bowlers[1]+"\n"+top10Bowlers[2]+"\n"+top10Bowlers[3]+"\n"+top10Bowlers[4]+"\n"+top10Bowlers[5]+"\n"+top10Bowlers[6]+"\n"+top10Bowlers[7]+"\n"+top10Bowlers[8]+"\n"+top10Bowlers[9]);
+System.out.println("the top ten ipl teams are:"+"\n"+ipltop10TeamNames[0]+"\n"+ipltop10TeamNames[1]+"\n"+ipltop10TeamNames[2]+"\n"+ipltop10TeamNames[3]+"\n"+ipltop10TeamNames[4]+"\n"+ipltop10TeamNames[5]+"\n"+ipltop10TeamNames[6]+"\n"+ipltop10TeamNames[7]+"\n"+ipltop10TeamNames[8]+"\n"+ipltop10TeamNames[9]);
+
+}
+}
